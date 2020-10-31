8b64f5ad687a0f2135714ed10e0a1cca5f7f5f39
littledian <1197434548@qq.com>
2020-10-31 10:36:46 +0800

    feat: update versions

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
-import NextDocument, { Head, Main, NextScript } from 'next/document';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import { DocumentProps as NextDocumentProps } from 'next/dist/next-server/lib/utils';
 import { getFooterBlock, BlockItem } from '@/services/block';
-import styles from './_document.scss';
 
 interface DocumentProps extends NextDocumentProps {
   block: {
@@ -22,12 +21,12 @@ export default class Document extends NextDocument<DocumentProps> {
     const { block } = this.props;
     const { footer } = block;
     return (
-      <html>
+      <Html>
         <Head>
           <link
             rel="stylesheet"
             type="text/css"
-            href="https://www.smsqsrg.cn/static/antd/4.6.6/antd.min.css"
+            href="https://unpkg.com/antd@4.7.3/dist/antd.min.css"
           />
           <style dangerouslySetInnerHTML={{ __html: footer.css.content }} />
         </Head>
@@ -38,17 +37,17 @@ export default class Document extends NextDocument<DocumentProps> {
             className="footer"
             dangerouslySetInnerHTML={{ __html: footer.html.content }}
           />
-          <script src="https://www.smsqsrg.cn/static/react/16.13.1/react.production.min.js" />
-          <script src="https://www.smsqsrg.cn/static/react-dom/16.13.1/react-dom.production.min.js" />
-          <script src="https://www.smsqsrg.cn/static/moment/2.29.1/moment.min.js" />
-          <script src="https://www.smsqsrg.cn/static/antd/4.6.6/antd-with-locales.min.js" />
+          <script src="https://unpkg.com/react@17.0.1/umd/react.production.min.js" />
+          <script src="https://unpkg.com/react-dom@17.0.1/umd/react-dom.production.min.js" />
+          <script src="https://unpkg.com/moment@2.29.1/min/moment-with-locales.min.js" />
+          <script src="https://unpkg.com/antd@4.7.3/dist/antd-with-locales.min.js" />
           <script
             {...(footer.js.attributes || {})}
             dangerouslySetInnerHTML={{ __html: footer.js.content }}
           />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
